f1ea7e6ec3ba67c8743330dd92e2f5d34433c31c
Justin Dority <justindority@gmail.com>
2024-02-09 14:25:13 -0600

    changes for publish

--- runsql.sql
+++ runsql.sql
@@ -8,3 +8,11 @@ WHERE 'name' = 'Bounty Hunger'
 
 SELECT id, name
 from dotaapi_hero
+
+UPDATE dotaapi_hero
+SET portraiturl = 'https://img-9gag-fun.9cache.com/photo/av5zwnq_700bwp.webp'
+WHERE name = 'Marci'
+
+DELETE FROM dotaapi_profile
+
+delete from auth_user
